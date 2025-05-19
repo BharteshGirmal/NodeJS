f4dda215c5b463bfc0544075ffa18a11fafc308f
BharteshGirmal <bharteshgirmal@gmail.com>
2025-05-19 22:40:04 +0530

    Modules in Node JS

---
+++ Basic/index.js
@@ -0,0 +1,7 @@
+const math = require("./math");
+
+console.log("------------- Node JS -------------");
+console.log("Addition : ", math.addtion(1, 2));
+console.log("subtraction : ", math.subtraction(13, 2));
+console.log("Multiplication : ", math.multiplication(10, 2));
+console.log("Division : ", math.division(67, 2));
---
+++ Basic/math.js
@@ -0,0 +1,17 @@
+function addtion(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  return a / b;
+}
+
+module.exports = { addtion, subtraction, multiplication, division };
