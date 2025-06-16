ebb70da0e59d17e5aeadd4e79e95912fd3acaba0
BharteshGirmal <bharteshgirmal@gmail.com>
2025-06-16 22:00:42 +0530

    mongodb-connection

--- REST API/index.js
+++ REST API/index.js
@@ -1,8 +1,52 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/rest-api")
+  .then(() => {
+    console.log("Connected to the MongoDB Server");
+  })
+  .catch((err) => console.log(err));
+
+// Schemas
+
+const userShema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    jobTitle: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+// Model
+
+const userObj = mongoose.model("user", userShema);
+
 // to update the JSON File Data.
 const fs = require("fs");
+const { type } = require("os");
+const { timeStamp } = require("console");
 
 const app = express();
 app.use(express.json());
@@ -27,7 +71,9 @@ app.use((req, res, next) => {
     }
   );
 });
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
+  const getUsers = await userObj.find();
+
   const table = `<table border="1">
   <thead>
   <th>First Name</th>
@@ -37,13 +83,13 @@ app.get("/users", (req, res) => {
   <th>Job Titiler</th>
   </thead>
   <tbody>
-  ${users
+  ${getUsers
     .map(
-      (user) => `<tr><td>${user.first_name}</td>
-    <td>${user.last_name}</td>
+      (user) => `<tr><td>${user.firstName}</td>
+    <td>${user.lastName}</td>
     <td>${user.gender}</td>
     <td>${user.email}</td>
-    <td>${user.job_title}</td></tr>`
+    <td>${user.jobTitle}</td></tr>`
     )
     .join("")}
   </tbody>
@@ -63,6 +109,14 @@ app.get("/api/user/:id", (req, res) => {
   return res.json(user);
 });
 
+app.get("/mongoapi/user/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await userObj.findById(id);
+
+  return res.json(user);
+});
+
 app.post("/api/user", (req, res) => {
   const body = req.body;
   console.log("Form Data : " + body);
@@ -75,6 +129,28 @@ app.post("/api/user", (req, res) => {
       return res.json({ status: "Success", ID: users.length });
     }
   });
+});
+
+app.post("/mongoapi/user", async (req, res) => {
+  const body = req.body;
+
+  const result = await userObj.create({
+    firstName: body.first_name,
+    lastName: body.last_name,
+    email: body.email,
+    gender: body.gender,
+    jobTitle: body.job_title,
+  });
+
+  return res.status(201).json({ status: "User Created Successfully" });
+
+  // fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+  //   if (err) {
+  //     res.json({ error: err });
+  //   } else {
+  //     return res.json({ status: "Success", ID: users.length });
+  //   }
+  // });
 });
 
 // UPDATE (Partial) user by ID
@@ -118,6 +194,17 @@ app.delete("/api/user/:id", (req, res) => {
         return res.json({ status: "Deleted", ID: id });
       }
     });
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
+
+app.delete("/mongoapi/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const index = userObj.findByIdAndDelete(id);
+
+  if (index) {
+    return res.json({ status: "Deleted", ID: id });
   } else {
     res.status(404).json({ error: "User not found" });
   }
